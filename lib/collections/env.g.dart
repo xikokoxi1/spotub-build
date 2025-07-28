438158c1dfd949efcd531b790027525e03551f81
xikokoxi <tiposic247@prorsd.com>
2025-07-28 12:54:22 -0300

    Ensure env.g.dart is included

---
+++ lib/collections/env.g.dart
@@ -0,0 +1,365 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _Env {
+  static const List<int> _enviedkeyrawSpotifySecrets = <int>[
+    3754733402,
+    4104550493,
+    1909769662,
+    1849024564,
+    2689664448,
+    3478400131,
+    2471807695,
+    628881527,
+    2973988157,
+    1104736872,
+    2567742221,
+    3238075455,
+    1291150574,
+    3147773244,
+    234791175,
+    182225256,
+    998892642,
+    2381789385,
+    2241538661,
+    1021032368,
+    3118773780,
+    1810324915,
+    921763767,
+    10018967,
+    314566715,
+    2508634439,
+    420275865,
+    1676176504,
+    3972467528,
+    3278866960,
+    3300633930,
+    355534427,
+    3747869218,
+    1183807186,
+    3661677536,
+    1015671283,
+    967442652,
+    491570542,
+    3521970078,
+    3511943420,
+    3056426605,
+    3355871800,
+    668882030,
+    3535711056,
+    964994614,
+    3872810783,
+    4161802439,
+    2210398449,
+    311639291,
+    748328927,
+    449357798,
+    1781762823,
+    813073164,
+    1242076871,
+    3919015245,
+    3242190639,
+    4045507704,
+    2575648518,
+    2395455660,
+    458198006,
+    4172803735,
+    3357070766,
+    3077977087,
+    1109100211,
+    2856859686,
+  ];
+
+  static const List<int> _envieddatarawSpotifySecrets = <int>[
+    3754733410,
+    4104550508,
+    1909769692,
+    1849024513,
+    2689664417,
+    3478400230,
+    2471807736,
+    628881478,
+    2973988110,
+    1104736863,
+    2567742268,
+    3238075481,
+    1291150554,
+    3147773277,
+    234791218,
+    182225243,
+    998892547,
+    2381789436,
+    2241538644,
+    1021032401,
+    3118773805,
+    1810324875,
+    921763797,
+    10019057,
+    314566749,
+    2508634403,
+    420275964,
+    1676176461,
+    3972467502,
+    3278866979,
+    3300633978,
+    355534394,
+    3747869208,
+    1183807207,
+    3661677520,
+    1015671232,
+    967442623,
+    491570443,
+    3521970094,
+    3511943370,
+    3056426505,
+    3355871757,
+    668881928,
+    3535711072,
+    964994644,
+    3872810795,
+    4161802480,
+    2210398401,
+    311639241,
+    748328934,
+    449357790,
+    1781762879,
+    813073213,
+    1242076926,
+    3919015291,
+    3242190617,
+    4045507659,
+    2575648567,
+    2395455645,
+    458197955,
+    4172803830,
+    3357070792,
+    3077976987,
+    1109100241,
+    2856859668,
+  ];
+
+  static final String rawSpotifySecrets = String.fromCharCodes(
+      List<int>.generate(
+    _envieddatarawSpotifySecrets.length,
+    (int i) => i,
+    growable: false,
+  ).map((int i) =>
+          _envieddatarawSpotifySecrets[i] ^ _enviedkeyrawSpotifySecrets[i]));
+
+  static const List<int> _enviedkeylastFmApiKey = <int>[
+    2454249799,
+    134371123,
+    936977561,
+    198900678,
+    3890522787,
+    2818142577,
+    4016982027,
+    1228263606,
+    1728844926,
+    1093570243,
+    623552505,
+    1319382577,
+    3804490351,
+    604501222,
+    2416573774,
+    2521996726,
+    2589473547,
+    3195437660,
+    1281642506,
+    2994565265,
+    3459970345,
+    2080113834,
+    4114631589,
+    379962764,
+    2250066970,
+    983746821,
+    1127121869,
+    3037806417,
+    247029656,
+    1659406061,
+    2606726688,
+    653048976,
+  ];
+
+  static const List<int> _envieddatalastFmApiKey = <int>[
+    2454249843,
+    134371073,
+    936977578,
+    198900647,
+    3890522773,
+    2818142532,
+    4016982078,
+    1228263566,
+    1728844872,
+    1093570213,
+    623552449,
+    1319382613,
+    3804490326,
+    604501205,
+    2416573823,
+    2521996673,
+    2589473646,
+    3195437668,
+    1281642553,
+    2994565282,
+    3459970331,
+    2080113810,
+    4114631616,
+    379962862,
+    2250067065,
+    983746915,
+    1127121917,
+    3037806439,
+    247029677,
+    1659405960,
+    2606726678,
+    653049076,
+  ];
+
+  static final String lastFmApiKey = String.fromCharCodes(List<int>.generate(
+    _envieddatalastFmApiKey.length,
+    (int i) => i,
+    growable: false,
+  ).map((int i) => _envieddatalastFmApiKey[i] ^ _enviedkeylastFmApiKey[i]));
+
+  static const List<int> _enviedkeylastFmApiSecret = <int>[
+    504711692,
+    1856171268,
+    4113003999,
+    2041668325,
+    2721303631,
+    2483294377,
+    4025326914,
+    832085538,
+    379385000,
+    3784106902,
+    1864081684,
+    2016477699,
+    2171874641,
+    318807231,
+    131774879,
+    529015241,
+    3195331082,
+    3213544023,
+    235225930,
+    399411131,
+    3220812416,
+    3316273499,
+    1143783794,
+    2171660152,
+    3662448671,
+    2207407413,
+    2375304753,
+    3319135233,
+    3699721106,
+    3700271333,
+    1677409996,
+    863169786,
+  ];
+
+  static const List<int> _envieddatalastFmApiSecret = <int>[
+    504711743,
+    1856171313,
+    4113003964,
+    2041668317,
+    2721303676,
+    2483294412,
+    4025326884,
+    832085524,
+    379384988,
+    3784106997,
+    1864081782,
+    2016477748,
+    2171874615,
+    318807257,
+    131774892,
+    529015293,
+    3195331123,
+    3213544038,
+    235225976,
+    399411086,
+    3220812470,
+    3316273517,
+    1143783696,
+    2171660104,
+    3662448678,
+    2207407365,
+    2375304787,
+    3319135331,
+    3699721124,
+    3700271324,
+    1677409967,
+    863169691,
+  ];
+
+  static final String lastFmApiSecret = String.fromCharCodes(List<int>.generate(
+    _envieddatalastFmApiSecret.length,
+    (int i) => i,
+    growable: false,
+  ).map(
+      (int i) => _envieddatalastFmApiSecret[i] ^ _enviedkeylastFmApiSecret[i]));
+
+  static final int _enviedkey_hideDonations = 4261321941;
+
+  static final int _hideDonations = _enviedkey_hideDonations ^ 4261321941;
+
+  static const List<int> _enviedkey_enableUpdateChecker = <int>[3477287560];
+
+  static const List<int> _envieddata_enableUpdateChecker = <int>[3477287609];
+
+  static final String _enableUpdateChecker = String.fromCharCodes(
+      List<int>.generate(
+    _envieddata_enableUpdateChecker.length,
+    (int i) => i,
+    growable: false,
+  ).map((int i) =>
+          _envieddata_enableUpdateChecker[i] ^
+          _enviedkey_enableUpdateChecker[i]));
+
+  static const List<int> _enviedkey_releaseChannel = <int>[
+    828603332,
+    2302694343,
+    3936406967,
+    3700992696,
+    2083012788,
+    1210819236,
+    236771005,
+  ];
+
+  static const List<int> _envieddata_releaseChannel = <int>[
+    828603306,
+    2302694318,
+    3936406992,
+    3700992720,
+    2083012800,
+    1210819272,
+    236771012,
+  ];
+
+  static final String _releaseChannel = String.fromCharCodes(List<int>.generate(
+    _envieddata_releaseChannel.length,
+    (int i) => i,
+    growable: false,
+  ).map(
+      (int i) => _envieddata_releaseChannel[i] ^ _enviedkey_releaseChannel[i]));
+
+  static const List<int> _enviedkey_disableSpotifyImages = <int>[3675946012];
+
+  static const List<int> _envieddata_disableSpotifyImages = <int>[3675946028];
+
+  static final String _disableSpotifyImages = String.fromCharCodes(
+      List<int>.generate(
+    _envieddata_disableSpotifyImages.length,
+    (int i) => i,
+    growable: false,
+  ).map((int i) =>
+          _envieddata_disableSpotifyImages[i] ^
+          _enviedkey_disableSpotifyImages[i]));
+}
